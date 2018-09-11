c71041708495e9db7a6e912a2e4299b66c367a82
klimentru1986 <kliment1986@inbox.ru>
2018-09-11 22:06:53 +0300

    components and models

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,19 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { MaterialModule } from './material/material.module';
+import { AddTodoComponent } from './components/add-todo/add-todo.component';
+import { TodoListComponent } from './components/todo-list/todo-list.component';
+import { TodoItemComponent } from './components/todo-item/todo-item.component';
 
 @NgModule({
-  declarations: [AppComponent],
-  imports: [BrowserModule, BrowserAnimationsModule],
+  declarations: [
+    AppComponent,
+    AddTodoComponent,
+    TodoListComponent,
+    TodoItemComponent
+  ],
+  imports: [BrowserModule, BrowserAnimationsModule, MaterialModule],
   providers: [],
   bootstrap: [AppComponent]
 })
---
+++ src/app/models/todo.model.ts
@@ -0,0 +1,4 @@
+export class ToDo {
+  id: number;
+  name: string;
+}

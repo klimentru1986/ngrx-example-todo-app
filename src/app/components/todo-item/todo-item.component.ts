49a95f2bb83f896580db589ee00fde221d58b1a1
klimentru1986 <kliment1986@inbox.ru>
2018-09-11 23:14:22 +0300

    remove todo

--- src/app/components/todo-item/todo-item.component.ts
+++ src/app/components/todo-item/todo-item.component.ts
@@ -1,9 +1,8 @@
-import {
-  Component,
-  ChangeDetectionStrategy,
-  Input
-} from '@angular/core';
+import { Component, ChangeDetectionStrategy, Input } from '@angular/core';
 import { ToDo } from '../../models/todo.model';
+import { ToDoState } from '../../store/todo.reducer';
+import { Store } from '@ngrx/store';
+import { RemoveToDo } from '../../store/todo.actions';
 
 @Component({
   selector: 'app-todo-item',
@@ -15,5 +14,9 @@ export class TodoItemComponent {
   @Input()
   toDo: ToDo;
 
-  constructor() {}
+  constructor(private store: Store<ToDoState>) {}
+
+  onRemoveClick(): void {
+    this.store.dispatch(new RemoveToDo(this.toDo.id));
+  }
 }

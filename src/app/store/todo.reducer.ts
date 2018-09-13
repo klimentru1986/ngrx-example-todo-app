a0e3d7426ff4106da4b546a55436a1b3ab87d2a3
Рудниченко Климент <kliment1986@inbox.ru>
2018-09-13 09:41:43 +0300

    add initial todos to entity

--- src/app/store/todo.reducer.ts
+++ src/app/store/todo.reducer.ts
@@ -1,18 +1,15 @@
+import { createEntityAdapter, EntityAdapter, EntityState } from '@ngrx/entity';
+import { INITIAL_TODOS } from '../const/initial-todos.const';
 import { ToDo } from '../models/todo.model';
 import { ToDoActions, ToDoActionTypes } from './todo.actions';
-import { INITIAL_TODOS } from '../const/initial-todos.const';
-import { EntityState, EntityAdapter, createEntityAdapter } from '@ngrx/entity';
 
 export interface ToDoState extends EntityState<ToDo> {}
 
 export const toDoAdapter: EntityAdapter<ToDo> = createEntityAdapter<ToDo>();
 
-const toDoInitialState: ToDoState = toDoAdapter.getInitialState();
+const toDoInitialState: ToDoState = toDoAdapter.getInitialState(INITIAL_TODOS);
 
-export function toDoReducer(
-  state = toDoInitialState,
-  action: ToDoActions
-): ToDoState {
+export function toDoReducer(state = toDoInitialState, action: ToDoActions): ToDoState {
   switch (action.type) {
     case ToDoActionTypes.AddToDo:
       return toDoAdapter.addOne(action.payload, state);
@@ -27,4 +24,3 @@ export function toDoReducer(
       return state;
   }
 }
-
